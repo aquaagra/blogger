011abe93242052fd72e373c9c11d784bab808163
fxrrr <aqua.agra.production@gmail.com>
2016-12-21 17:50:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -39,3 +39,36 @@ localStorage.CART = localStorage.CART || "[]";
 
 
 
+
+var caraPemesanan=
+`
+<br/>
+<br/>
+
+<h3>Pemesanan offline</h3>
+Silakan datang langsung ke outlet kami.<br/>
+Senin-Sabtu 09.00 s/d 18.00<br/>
+<br/>
+
+<h3>Pemesanan Online</h3>
+1. Cek harga dan jenis pin terlebih dahulu klik di sini , harga bisa nego dg CS kami<br/>
+2. Kirim desain via email<br/>
+3. Konfirmasi orderan kontak seluler kami<br/>
+4. Jika sudah fix desain dan jumlah orderan silakan transfer DP 50% dari total pembayaran*<br/>
+5. Jika barang sudah selesai diproduksi kami akan kirimkan fotonya<br/>
+6. Pelanggan melunasi pembayaran sebelum barang dikirim<br/>
+7. Barang dikirim ke alamat Anda via jasa pengiriman yang disepakati<br/>
+<br/>
+
+<h3>Pengiriman</h3>
+<h4>Cek harga pengiriman ke kota Anda</h4>
+1. Pos Indonesia (<a href='http://www.posindonesia.co.id' target='blank' rel='nofollow'>www.posindonesia.co.id</a>)<br/>
+2. JNE (<a href='http://jne.co.id/' target='blank' rel='nofollow'>jne.co.id</a>)<br/>
+3. Gojek (<a href='https://www.go-jek.com/blog' target='blank' rel='nofollow'>go-jek.com/blog</a>)<br/>
+4. Atau jasa pengiriman lainnya yang disepakati<br/>
+
+
+`;
+
+
+
