18f9cb4b4164a4f8943e25c521ebfe286bdd7bb9
fxrrr <aqua.agra.production@gmail.com>
2016-12-21 16:45:54 +0700

    Create config-aag.js

---
+++ config-aag.js
@@ -0,0 +1,41 @@
+
+
+
+
+
+var CONFIG={};
+
+  CONFIG.menu={
+    'Home':'',
+    'Instagram':'',
+    'Products':{
+      'Percetakan':[
+        'Kartu Nama,kartu-nama',
+        'Brosur,brosur',
+        'Spanduk,spanduk',
+        'Kalender,kalender',
+        'Undangan,undangan'
+      ],
+      'Konveksi':[
+        'Kaos,',
+        'Blazer,',
+        'Jacket,'
+      ],
+      'Souvenir':[
+        'Jam,',
+        'Pulpen,',
+        'Gantungan Kunci,',
+        'Mug,'
+      ]
+    },
+    'Daftar Harga':'6482945127747674215',
+    'Cara Pemesanan':'6257960933882626139',
+    'Cara Pembayaran':'6257960933882626139',
+    'Pengiriman':'6257960933882626139'
+  };
+
+
+
+
+
+
